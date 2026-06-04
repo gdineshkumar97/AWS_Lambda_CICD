53ee49e5b176460685266e1cc29ec51d5a7e6304
Dineshkumar G <gdineshkumar006@gmail.com>
2026-06-04 23:53:53 +0530

    changed

--- lambda_function/app.py
+++ lambda_function/app.py
@@ -5,7 +5,7 @@ import os
 
 def lambda_handler(event, context):
     
-    print("Deployment via CICD")
+    print("Deployment via CICD Additional Changes")
     print("Event received:", event)
     response = requests.get("https://api.covid19api.com/summary")
     data = response.json()
